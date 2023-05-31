3560e6e1f7b4b608366f33a5d18162dffefde65c
islate <darkslate115@gmail.com>
2023-05-31 19:13:59 +0500

    cinephile

---
+++ src/components/Upcoming/Upcoming.vue
@@ -0,0 +1,19 @@
+<template>
+  <Transition name="upcoming" mode="out-in">
+    <div class="main__upcoming">
+      <UpcomingItem />
+    </div>
+  </Transition>
+</template>
+
+<script setup>
+import UpcomingItem from "@/components/Upcoming/UpcomingItem.vue";
+import { useUpcoming } from "@/stores/upcoming.js";
+import { onMounted, ref } from "vue";
+let upcomingStore = useUpcoming();
+
+onMounted(() => {
+  upcomingStore.getUpcoming();
+});
+</script>
+
